9b324852ef06721e68c31b535ecd63efd9abad2a
George Fan <georgefan112358@gmail.com>
2026-01-24 15:32:51 -0500

    Test simulate driving and i2c 8 motors

--- click_a_mole/motor_interface/motor_interface.ino
+++ click_a_mole/motor_interface/motor_interface.ino
@@ -22,14 +22,14 @@ void setup() {
     )
   );
 
-  // // controller.addModule(
-  // //   new MoleModule(
-  // //     {24, &PORTA, (1 << PA2)},   // pul
-  // //     {25, &PORTA, (1 << PA3)},   // dir
-  // //     31,                         // button
-  // //     1                           // mux channel
-  // //   )
-  // // );
+  controller.addModule(
+    new MoleModule(
+      {24, &PORTA, (1 << PA2)},   // pul
+      {25, &PORTA, (1 << PA3)},   // dir
+      31,                         // button
+      1                           // mux channel
+    )
+  );
 
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.println("Setup init");
@@ -39,39 +39,47 @@ void setup() {
   Serial.println("Setup complete");
 }
 
-int hp[2] = {5, 5};
-int buttons[2];
+int hp[8] = {0};
+int buttons[8];
 int i = 0;
 void loop() {
-  if (i % 10 == 0) {
-    Serial.println("-----");
-    Serial.print("Loop: ");
-    Serial.println(i);
-  }
+  // if (i % 10 == 0) {
+  //   Serial.println("-----");
+  //   Serial.print("Loop: ");
+  //   Serial.println(i);
+  // }
 
   controller.updateAll();
   controller.readButtons(buttons);
 
-  if (buttons[0]) hp[0] = (hp[0] + 1) % 10;
-  if (buttons[1]) hp[1] = (hp[1] + 1) % 10;
-
-  controller.setHp(0, hp[0], 10);
-  controller.setHp(1, hp[1], 10);
-  
-  if (i % 10 == 0) {
-    Serial.print((hp[0]/10)*360);
-
-    Serial.print("Buttons: ");
-    Serial.print(buttons[0]);
-    Serial.print(", ");
-    Serial.println(buttons[1]);
-
-    Serial.print("HP: ");
-    Serial.print(hp[0]);
-    Serial.print(", ");
-    Serial.println(hp[1]); 
+  for (int j = 0; j < 8; j++) {
+    if (i % 100 == 0) hp[j] = (hp[j] + 1) % 10;
+    controller.setHp(j, hp[j], 10);
   }
 
+  // if (buttons[0]) hp[0] = (hp[0] + 1) % 10;
+  // if (buttons[1]) hp[1] = (hp[1] + 1) % 10;
+
+  // if (i%200==0) hp[0] = (hp[0] + 1) % 10;
+  // if (i%200==0) hp[1] = (hp[1] + 1) % 10;
+
+  // controller.setHp(0, hp[0], 10);
+  // controller.setHp(1, hp[1], 10);
+  
+  // if (i % 10 == 0) {
+  //   Serial.print((hp[0]/10)*360);
+
+  //   Serial.print("Buttons: ");
+  //   Serial.print(buttons[0]);
+  //   Serial.print(", ");
+  //   Serial.println(buttons[1]);
+
+  //   Serial.print("HP: ");
+  //   Serial.print(hp[0]);
+  //   Serial.print(", ");
+  //   Serial.println(hp[1]); 
+  // }
+
   i++;
-  delay(10);
+  delay(5);
 }
