44d63a6c971a3d77de73b49c1ec2640e8588365a
tabetant <antoine.tabet@mail.utoronto.ca>
2026-01-24 17:04:58 -0500

    added changes to ino file

--- gamelogic_final/click-a-mole.ino
+++ gamelogic_final/click-a-mole.ino
@@ -1,18 +1,18 @@
 #include "gamelogic.h"
 #include "led_interface.h"
 #include "motor_interface.h"
-using namespace std;
+
 DisplayInterface *p_di = nullptr;
 MotorInterface *p_mi = nullptr;
 GameLogic *game = nullptr;
-setup()
+void setup()
 {
     Serial.begin(9600);
     p_di = new DisplayInterface; // Placeholder, replace with actual DisplayInterface object
     p_mi = new MotorInterface;   // Placeholder, replace with actual MotorInterface
     game = new GameLogic(p_di, p_mi);
 }
-loop()
+void loop()
 {
     game->fsm();
 }
